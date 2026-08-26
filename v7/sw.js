f3d90adf4ee417673ed845521c50ffa36c2ba195
bheshmatioei-ctrl <b.heshmati.oei@gmail.com>
2026-08-26 14:20:09 +0200

    Cache V7 diagnostics layer

--- v7/sw.js
+++ v7/sw.js
@@ -1,5 +1,5 @@
-const CACHE='market-v7-shell-v4';
-const ASSETS=['./','./index.html','./styles-v7.css','./app-v7.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='market-v7-shell-v5';
+const ASSETS=['./','./index.html','./styles-v7.css','./app-v7.js','./qa-fixes-v7.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match(e.request).then(r=>r||(e.request.mode==='navigate'?caches.match('./index.html'):Response.error()))))});
